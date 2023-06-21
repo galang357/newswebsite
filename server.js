02e1db5324d0470a6cbaa24499e138df84130ca9
znghifari <zainalghifari28@gmail.com>
2023-06-21 22:12:37 +0800

    opsi wajib input artikel

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const http = require('http');
-const port = 3000; // Port yang akan digunakan oleh server
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!');
-  
+  // Mendapatkan path file yang diminta
+  const filePath = path.join(__dirname, req.url);
+
+  // Membaca file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      // Jika terjadi kesalahan (misalnya file tidak ditemukan), kirim status 404
+      res.statusCode = 404;
+      res.end('File not found');
+    } else {
+      // Jika file berhasil dibaca, kirimkan kontennya dengan tipe MIME yang sesuai
+      res.setHeader('Content-Type', 'text/html');
+      res.end(content);
+    }
+  });
 });
 
 server.listen(port, () => {
